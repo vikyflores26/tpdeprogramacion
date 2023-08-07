65a0269a2f155c4d33160716ac04c8a05bb4c360
alumno <alumno@Instituto.local>
2023-08-07 10:55:05 -0300

    cambio2

--- clase1.js
+++ clase1.js
@@ -5,5 +5,15 @@ console.log ( 60 / 20 )
 let suma = 30 + 20 
 let resta = 50 - 20 
 let multiplicacion = 10 * 20 
-let divison = 60 / 20 
+let division = 60 / 20 
 
+alert ( 'el resultado de la suma es ' +  ( suma ))
+alert ( 'el resultado de  la resta  es '+ ( resta ))
+alert (' el resultado de la multiplicacion ' + ( multiplicacion ) )  
+alert ( ' el resultado de la divison es' + ( division  ))
+ 
+alert ( ' mi edad es :' + ( 2023 - 2008 ))
+
+let nombrealumno 
+nombrealumno = prompt ( ' ingresa tu nombre ')
+alert  ( ' bienvenido '+ nombrealumno)
