d51e65cc9f6709aa2a08055d9721396926e53b94
alumno <alumno@Instituto.local>
2023-08-07 10:42:37 -0300

    cambios

---
+++ clase1.js
@@ -0,0 +1,9 @@
+console.log ( 30 + 20 )
+console.log ( 50 - 20  )
+console.log ( 10 * 20 )
+console.log ( 60 / 20 )
+let suma = 30 + 20 
+let resta = 50 - 20 
+let multiplicacion = 10 * 20 
+let divison = 60 / 20 
+
